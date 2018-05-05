4316aa10646f8d8891c088af7224f631174583af
pc <Patrick.Christ.91@web.de>
2018-05-05 15:39:45 +0200

    implemented CSVFootballParser

--- src/main/java/de/exxcellent/challenge/parser/CSVFootballParser.java
+++ src/main/java/de/exxcellent/challenge/parser/CSVFootballParser.java
@@ -1,7 +1,13 @@
 package de.exxcellent.challenge.parser;
 
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import de.exxcellent.challenge.model.FootballEntry;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -10,8 +16,43 @@ import java.util.List;
  */
 public class CSVFootballParser implements FileParser<FootballEntry> {
 
+    private static final String[] CSV_HEADER = {"Team", "Games", "Wins", "Losses", "Draws", "Goals", "GoalsAllowed", "Points"};
+
     @Override
     public List<FootballEntry> parseFile(String filePath) {
-        return null;
+
+        BufferedReader fileReader = null;
+        CsvToBean<FootballEntry> csvToBean;
+        List<FootballEntry> result = null;
+
+        try {
+            fileReader = new BufferedReader(new FileReader(filePath));
+
+            ColumnPositionMappingStrategy<FootballEntry> mappingStrategy =
+                    new ColumnPositionMappingStrategy<>();
+
+            mappingStrategy.setType(FootballEntry.class);
+            mappingStrategy.setColumnMapping(CSV_HEADER);
+
+            csvToBean = new CsvToBeanBuilder<FootballEntry>(fileReader)
+                    .withMappingStrategy(mappingStrategy)
+                    .withSkipLines(1)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            result = csvToBean.parse();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
