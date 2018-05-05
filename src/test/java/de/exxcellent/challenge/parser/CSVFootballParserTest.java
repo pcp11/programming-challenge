0107b782fcc94929e3a78912898ce326e8963cde
pc <Patrick.Christ.91@web.de>
2018-05-05 15:28:10 +0200

    added test for CSVFootballParser

---
+++ src/test/java/de/exxcellent/challenge/parser/CSVFootballParserTest.java
@@ -0,0 +1,42 @@
+package de.exxcellent.challenge.parser;
+
+import de.exxcellent.challenge.App;
+import de.exxcellent.challenge.model.FootballEntry;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author pc
+ * @since 05.05.18
+ */
+public class CSVFootballParserTest {
+
+    private CSVFootballParser csvFootballParser;
+
+    private FootballEntry footballEntryA;
+    private FootballEntry footballEntryB;
+
+    @Before
+    public void setUp() throws Exception {
+        csvFootballParser = new CSVFootballParser();
+
+        footballEntryA = new FootballEntry("Arsenal",38,26,9,3,79,36,87);
+        footballEntryB = new FootballEntry("Tottenham",38,14,8,16,49,53,50);
+    }
+
+    @Test
+    public void parseFile() {
+        List<FootballEntry> footballEntries =  csvFootballParser.parseFile(App.footballCSV);
+
+        Assert.assertEquals(footballEntryA, footballEntries.get(0));
+        Assert.assertEquals(footballEntryB, footballEntries.get(8));
+
+        Assert.assertNotEquals(footballEntryA, footballEntries.get(1));
+        Assert.assertNotEquals(footballEntryB, footballEntries.get(7));
+    }
+}
